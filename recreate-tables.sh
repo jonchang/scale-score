3387c6796806e534dae1bb8025ef7943b7dd0331
Jonathan Chang <me@jonathanchang.org>
2024-04-17 08:57:59 -0700

    Safety for production deploys

--- recreate-tables.sh
+++ recreate-tables.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo 'THIS WILL DESTROY PRODUCTION DATABASES'
+echo 'CTRL-C to EXIT'
+read -rp "Press enter to continue: "
+echo ""
+echo "Waiting 10 seconds..."
+sleep 10
+
 npx wrangler d1 execute d1-example --remote --file schemas/schema.sql
 parallel -P1 -n1 npx wrangler d1 execute d1-example --remote --file {} ::: schemas/inserts-*.sql
