3fa4fc01e42231a083d13618dfeb8357b1b60ef5
Jonathan Chang <me@jonathanchang.org>
2024-04-17 08:48:36 -0700

    Add timestamp field

--- schemas/schema.sql
+++ schemas/schema.sql
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS ratings (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     basename TEXT NOT NULL,
     rater TEXT NOT NULL,
-    rating INTEGER NOT NULL
+    rating INTEGER NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE INDEX idx_ratings_basename ON ratings (basename);
