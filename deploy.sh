8efad9de6b7e9e4739f863df953a3069639be45e
Jonathan Chang <me@jonathanchang.org>
2024-04-17 08:58:29 -0700

    Also deploy wrangler

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ pushd _site
     gem exec neocities push --prune -e 1 -e 2 -e 3 -e 4 -e 5 -e 6 -e 7 -e 8 -e 9 -e a -e b -e c -e d -e e -e f -e g -e h -e i -e j -e k -e l -e m -e n -e o -e p -e q -e r -e s -e t -e u -e v -e w -e x -e y -e z .
 popd
 
-
+npx wrangler deploy
