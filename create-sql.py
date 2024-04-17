8c9e8b325d14be32a8f239e8d16088baa041efda
Jonathan Chang <me@jonathanchang.org>
2024-04-17 00:13:04 -0700

    Shard insert statements

--- create-sql.py
+++ create-sql.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-import itertools
 import shutil
 from pathlib import Path
+from collections import defaultdict
 
 base = Path("/Users/jonchang/Library/CloudStorage/Box-Box/image-segmentation")
 p1 = base / "Miyazawa_fishpix_segmented"
 p2 = base / "Miyazawa_fishbase_segmented"
+
+all_files = set(p1.iterdir()) | set(p2.iterdir())
 
 dest = Path("_site")
 dest.mkdir(parents=True, exist_ok=True)
@@ -14,13 +16,14 @@ dest.mkdir(parents=True, exist_ok=True)
 for idx in "0123456789abcdefghijklmnopqrstuvwxyz":
     (dest / idx).mkdir(parents=True, exist_ok=True)
 
-basenames = []
-for path in itertools.chain(p1.iterdir(), p2.iterdir()):
+basenames = defaultdict(list)
+for path in all_files:
     bn = path.name
-    basenames.append(f"('{bn}')")
     shard = dest / bn[0]
+    basenames[bn[0]].append(f"('{bn}')")
     shutil.copy(path, shard)
 
-insert_str = ", ".join(basenames)
-
-Path("schemas/inserts.sql").write_text(f"INSERT INTO IMAGES (basename) VALUES {insert_str};")
+with open(Path("schemas/inserts.sql"), "w") as wfile:
+    for inserts in basenames.values():
+        insert_str = ", ".join(inserts)
+        wfile.write(f"INSERT INTO IMAGES (basename) VALUES {insert_str};\n")
