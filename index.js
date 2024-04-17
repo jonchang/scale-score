d7a2e26e870ad4734b6a880e8c673901c8301fb8
Jonathan Chang <me@jonathanchang.org>
2024-04-17 00:54:59 -0700

    Add form for rating code

---
+++ index.js
@@ -0,0 +1,57 @@
+const btn_good = document.getElementById("btn-good");
+const btn_bad = document.getElementById("btn-bad");
+const input_basename = document.getElementById("basename");
+const input_rater = document.getElementById("rater");
+const img = document.getElementById("image-display")
+
+const post_url = "https://scale-score.jonchang.workers.dev/api/rate/"
+const get_url = "https://scale-score.jonchang.workers.dev/api/next"
+const img_prefix = "https://scale-score.neocities.org/"
+
+function update_img() {
+    const response = fetch(get_url).then(response => {
+        response.json().then(js => {
+            const basename = js[0].basename;
+            const img_url = img_prefix + basename[0] + "/" + basename;
+            img.src = img_url;
+            input_basename.value = basename;
+        })
+    })
+}
+
+function evt_submit(evt) {
+    evt.preventDefault();
+    if (!input_rater.value) {
+        alert("must say who you are");
+        return;
+    }
+
+    const btn = event.currentTarget;
+    let rating = 0;
+    if (btn.id == "btn-good") {
+        rating = 1;
+    }
+    let to_submit = {
+        rater: input_rater.value,
+        rating: rating
+    }
+    const submit_url = post_url + input_basename.value;
+
+    return fetch(submit_url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(to_submit)
+    }).then(resp => {
+        if (!resp.ok) {
+            resp.text().then(msg => { throw new Error(msg) });
+        }
+        update_img()
+    });
+}
+
+document.addEventListener('DOMContentLoaded', update_img);
+btn_good.addEventListener('click', evt_submit);
+btn_bad.addEventListener('click', evt_submit);
