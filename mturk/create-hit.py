65efac25492dd282d362216d443afacae1824ba5
Jonathan Chang <me@jonathanchang.org>
2024-05-04 11:59:18 -0700

    Minor refactor for sandbox

--- mturk/create-hit.py
+++ mturk/create-hit.py
@@ -1,15 +1,20 @@
 import boto3
 
+live_data = {
+    'endpoint_url': 'https://mturk-requester.us-east-1.amazonaws.com',
+    'hit_type': '3TNRXJHW8K9I7SWZ2BBOCHR6UGSC21'
+}
 
-# endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
-endpoint_url = 'https://mturk-requester.us-east-1.amazonaws.com'
+sandbox_data = {
+    'endpoint_url': 'https://mturk-requester-sandbox.us-east-1.amazonaws.com',
+    'hit_type': '3DY668G4EOVY56JAMRX2YD162DFXEW'
+}
 
-# hit_type = '3DY668G4EOVY56JAMRX2YD162DFXEW' # sandbox
-hit_type = '3TNRXJHW8K9I7SWZ2BBOCHR6UGSC21' # live
+data = sandbox_data
 
 client = boto3.client(
     'mturk',
-    endpoint_url=endpoint_url,
+    endpoint_url=data['endpoint_url'],
     region_name='us-east-1',
 )
 
@@ -19,6 +24,6 @@ NUM_HITS = 100
 question = open('external-question.xml',mode='r').read()
 print("HITGroupId\tHITTypeId\tHITId")
 for idx in range(NUM_HITS):
-    new_hit = client.create_hit_with_hit_type(MaxAssignments=4, HITTypeId=hit_type, LifetimeInSeconds=3600, Question=question)
+    new_hit = client.create_hit_with_hit_type(MaxAssignments=4, HITTypeId=data['hit_type'], LifetimeInSeconds=3600, Question=question)
     hh = new_hit['HIT']
     print(hh['HITGroupId'] + "\t" + hh['HITTypeId'] + "\t" + hh['HITId'])
